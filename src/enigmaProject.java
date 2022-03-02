e12579de345938961b7feeaca718e60f4af75812
jonx1183 <jonx1183@stud.kea.dk>
2022-03-02 10:44:16 +0100

    Second layout

--- src/enigmaProject.java
+++ src/enigmaProject.java
@@ -2,25 +2,27 @@ import java.util.Scanner;
 
 public class enigmaProject {
   Scanner in = new Scanner(System.in);
-  String text;
-  int shift;
+
 
   public static void main(String[] args) {
     new enigmaProject().go();
   }
 
   public void go(){
-    ceasarCipherInput();
+    String text = inputTextToIncrypt();
+    int shift = inputShift();
 
   }
 
-  public void ceasarCipherInput(){
-    System.out.print("Please enter text to encrypt:");
-    text = in.nextLine();
+  private String inputTextToIncrypt() {
 
-    System.out.print("Enter shift value: ");
-    shift = in.nextInt();
+    String textInput =in.nextLine();
+    return textInput;
   }
 
+  private int inputShift() {
 
+    int shiftInPut = in.nextInt();
+    return shiftInPut;
+  }
 }
